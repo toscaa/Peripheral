97c67ae939c05c42b52170758318f48c69c7bbf3
toscaa <christopher.hasan2711@gmail.com>
2023-05-24 11:34:06 +0700

    Menambahkan Form login, register dan function

--- User/add_user.php
+++ User/add_user.php
@@ -7,72 +7,81 @@ require_once "../Template/header.php";
 require_once "../Template/navbar.php";
 require_once "../Template/sidebar.php";
 
+//Jika Tombol Simpan Regis Di klik
+if (isset($_POST['simpan'])) {
+  registrasi($_POST);
+}
+
 ?>
 
 <div id="layoutSidenav_content">
   <main>
+
     <div class="container-fluid px-4">
       <h1 class="mt-4">Tambah User</h1>
       <ol class="breadcrumb mb-4">
         <li class="breadcrumb-item"><a href="../index.php">Home</a></li>
         <li class="breadcrumb-item active">Tambah User</li>
       </ol>
-      <div class="card">
-        <div class="card-header">
-          <span class="h5"><i class="fa-solid fa-square-plus"></i> Tambah User</span>
-          <button type="submit" name="simpan" class="btn btn-primary float-end"><i class="fa-solid fa-floppy-disk"></i> Simpan</button>
-          <button type="reset" name="reset" class="btn btn-danger float-end me-1"><i class="fa-solid fa-xmark"></i> Reset</button>
-        </div>
-        <div class="card-body">
-          <div class="row">
-            <div class="col-8">
-              <div class="mb-3 row">
-                <label for="username" class="col-sm-3 col-form-label">Username</label>
-                <label class="col-sm-1 col-form-label ">:</label>
-                <div class="col-sm-8" style="margin-left: -40px;">
-                  <input type="text" pattern="[A-Za-z0-9]{3,}" title="Minimal 3 Karakter kombinasi huruf besar dan angka" class="form-control border-0 border-bottom" id="username" name="username" placeholder="Masukkan Username Anda" maxlength="25">
+      <form action="" method="post">
+        <div class="card">
+          <div class="card-header">
+            <span class="h5"><i class="fa-solid fa-square-plus"></i> Tambah User</span>
+            <button type="submit" name="simpan" class="btn btn-primary float-end"><i class="fa-solid fa-floppy-disk"></i> Simpan</button>
+            <button type="reset" name="reset" class="btn btn-danger float-end me-1"><i class="fa-solid fa-xmark"></i> Reset</button>
+          </div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col-8">
+                <div class="mb-3 row">
+                  <label for="username" class="col-sm-3 col-form-label">Username</label>
+                  <label class="col-sm-1 col-form-label ">:</label>
+                  <div class="col-sm-8" style="margin-left: -40px;">
+                    <input required autofocus autocomplete="off" type="text" pattern="[A-Za-z0-9]{3,}" title="Minimal 3 Karakter kombinasi huruf besar dan angka" class="form-control border-0 border-bottom" id="username" name="username" placeholder="Masukkan Username Anda" maxlength="25">
+                  </div>
+                </div>
+                <div class="mb-3 row">
+                  <label for="username" class="col-sm-3 col-form-label">Nama</label>
+                  <label class="col-sm-1 col-form-label ">:</label>
+                  <div class="col-sm-8" style="margin-left: -40px;">
+                    <input required autocomplete="off" type="text" class="form-control border-0 border-bottom" id="nama" name="nama" placeholder="Masukkan Nama Anda">
+                  </div>
+                </div>
+                <div class="mb-3 row">
+                  <label for="username" class="col-sm-3 col-form-label">Roles</label>
+                  <label class="col-sm-1 col-form-label ">:</label>
+                  <div class="col-sm-8" style="margin-left: -40px;">
+                    <select required autocomplete="off" name="roles" id="roles" class="form-select border-0 border-bottom">
+                      <option value="" selected>--Pilih Roles--</option>
+                      <option value="Admin">Administrator</option>
+                      <option value="User">User</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="mb-3 row">
+                  <label for="password" class="col-sm-3 col-form-label">Password</label>
+                  <label class="col-sm-1 col-form-label ">:</label>
+                  <div class="col-sm-8" style="margin-left: -40px;">
+                    <input required type="password" pattern="[A-Za-z0-9]{3,}" title="Minimal 3 Karakter kombinasi huruf besar dan angka" class="form-control border-0 border-bottom" id="password1" name="password1" placeholder="Masukkan Password Anda" maxlength="25">
+                  </div>
+                </div>
+                <div class="mb-3 row">
+                  <label for="password" class="col-sm-3 col-form-label"> Komfirmasi</label>
+                  <label class="col-sm-1 col-form-label ">:</label>
+                  <div class="col-sm-8" style="margin-left: -40px;">
+                    <input required type="password" pattern="[A-Za-z0-9]{3,}" title="Minimal 3 Karakter kombinasi huruf besar dan angka" class="form-control border-0 border-bottom" id="password2" name="password2" placeholder="Masukkan Password Anda Lagi" maxlength="25">
+                  </div>
                 </div>
               </div>
-              <div class="mb-3 row">
-                <label for="username" class="col-sm-3 col-form-label">Nama</label>
-                <label class="col-sm-1 col-form-label ">:</label>
-                <div class="col-sm-8" style="margin-left: -40px;">
-                  <input type="text" class="form-control border-0 border-bottom" id="nama" name="nama" placeholder="Masukkan Nama Anda">
-                </div>
-              </div>
-              <div class="mb-3 row">
-                <label for="username" class="col-sm-3 col-form-label">Roles</label>
-                <label class="col-sm-1 col-form-label ">:</label>
-                <div class="col-sm-8" style="margin-left: -40px;">
-                  <select name="roles" id="roles" class="form-select border-0 border-bottom">
-                    <option value="" selected>--Pilih Roles--</option>
-                    <option value="Admin">Administrator</option>
-                    <option value="User">User</option>
-                  </select>
-                </div>
-              </div>
-              <div class="mb-3 row">
-                <label for="password" class="col-sm-3 col-form-label">Password</label>
-                <label class="col-sm-1 col-form-label ">:</label>
-                <div class="col-sm-8" style="margin-left: -40px;">
-                  <input type="password" pattern="[A-Za-z0-9]{3,}" title="Minimal 3 Karakter kombinasi huruf besar dan angka" class="form-control border-0 border-bottom" id="password" name="password" placeholder="Masukkan Password Anda" maxlength="25">
-                </div>
-              </div>
-              <div class="mb-3 row">
-                <label for="password" class="col-sm-3 col-form-label"> Komfirmasi</label>
-                <label class="col-sm-1 col-form-label ">:</label>
-                <div class="col-sm-8" style="margin-left: -40px;">
-                  <input type="password" pattern="[A-Za-z0-9]{3,}" title="Minimal 3 Karakter kombinasi huruf besar dan angka" class="form-control border-0 border-bottom" id="password" name="password" placeholder="Masukkan Password Anda Lagi" maxlength="25">
-                </div>
-              </div>
-            </div>
-            <div class="col-4">
+              <div class="col-4">
 
+              </div>
             </div>
           </div>
         </div>
-      </div>
     </div>
+    </form>
+
   </main>
   <?php
 
--- function.php
+++ function.php
@@ -1,12 +1,53 @@
-<?php 
-$conn = mysqli_connect('localhost', 'root', '', 'peripheral');
+<?php
 
-// //Cek koneksi 
-// if (mysqli_connect_errno()){
-//   echo "Gagal koneksi Ke DB";
-// } else {
-//   echo "Berhasil Koneksi";
-// }
+function koneksi()
+{
+  return mysqli_connect('localhost', 'root', '', 'peripheral');
+}
+
+function query($query)
+{
+  $conn = koneksi();
+  $result = mysqli_query($conn, $query);
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+  return $rows;
+}
+
+function registrasi($data)
+{
+  $conn = koneksi();
+
+  $username = htmlspecialchars(strtolower($data['username']));
+  $nama = htmlspecialchars(strtolower($data['nama']));
+  $roles = htmlspecialchars(strtolower($data['roles']));
+  $password1 = mysqli_real_escape_string($conn, $data['password1']);
+  $password2 = mysqli_real_escape_string($conn, $data['password2']);
+
+  $alert = '';
+
+  //Cek username jika sudah ada
+  if (query("SELECT * FROM tbl_user WHERE username = '$username'")) {
+    echo "<script>
+            alert ('Username yang anda masukkan sudah terdaftar !');
+            document.location.href = 'add_user.php';
+          </script>";
+    return false;
+  }
+
+  //Jika Komfirmasi password tidak sesuai
+  if ($password1 !== $password2) {
+    echo "<script>
+            alert ('Komfirmasi password tidak sesuai');
+            document.location.href = 'add_user.php';
+          </script>";
+    return false;
+  }
+
+}
 
 
 $main_url = "http://localhost/Peripheral/";
